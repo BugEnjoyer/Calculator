437b27c356f8ae316ce7c5234bdf4a90c3ada45d
kevinR <kevinroesner94@gmail.com>
2024-06-02 18:46:21 +0200

    The logic is nearly done

--- calculator.js
+++ calculator.js
@@ -1,10 +1,17 @@
 
-let operator = null;
-let num1 = 10;
-let num2 = 20;
+let operator = '';
+let num1 = '';
+let num2 = '';
+let currentValue = '';
 
-function add ( num1, num2 ) {
-  return num1 + num2;
+function add ( num1, num2, currentValue ) {
+  if ( currentValue > 0 ) {
+    currentValue + Number(num2);
+    return currentValue;
+  }else{
+  currentValue = Number(num1) + Number(num2);
+  }
+  return currentValue;
 }
 
 function subtract ( num1, num2 ) {
@@ -19,13 +26,13 @@ function divide ( num1, num2 ) {
   return num1 / num2;
 }
 
-function operate(num1, op, num2) {
+function operate(num1, op, num2, currentValue) {
   if ( op === '+') {
-    return add( num1, num2 );
+    return add( num1, num2, currentValue );
   } else if ( op === '-') {
-    return subtract( num1, num2 );
+    return subtract( num1, num2, currentValue );
   }else if ( op === '*') {
-    return multiply( num1, num2 );
+    return multiply( num1, num2, currentValue );
   }else if ( op === '/') {
     return divide ( num1, num2 );
   }else if ( op === 0 ) {
@@ -43,19 +50,50 @@ const opButton = document.querySelectorAll('.operator-button');
 
  let display = document.querySelector('.display-container');
  function updateDisplay() {
-  display.innerText = `${operator}`;
+  if ( currentValue === '') {
+  display.textContent = num1 + `${operator}` + num2;
+  }else if( currentValue !== '') {
+    display.textContent = currentValue + operator + num2;
+  }
  }
  updateDisplay();
- console.log(operate(num1,'+',num2));
 
 const numberButtons = document.querySelectorAll('.value-button');
 numberButtons.forEach( button => {
   button.addEventListener('click', event => {
-    if ( operator === null ) {
-      num1 += button.innerText;
+    if ( operator === '' ) {
+      num1 += event.target.textContent;
+      updateDisplay();
+      console.log ( num1 );
+    }else if ( operator !== '' ) {
+      num2 += event.target.textContent;
+      updateDisplay();
+      console.log( num2 );
     }
   });
 });
  
+const calc= document.querySelector('.calculate-button');
+calc.addEventListener('click', () => {
+  doCalculate();
+  updateDisplay();
+  console.log(currentValue);
+});
+
+function doCalculate() {
+  if ( currentValue === '' && num2 !== '' ) {
+    currentValue = operate(num1,operator,num2, currentValue);
+    operator = '';
+    num2 = '';
+    num1 = '';
+  }else if ( currentValue !== '') {
+    currentValue = operate(num1,operator,num2, currentValue);
+    operator = '';
+    num2 = '';
+  }
+}
+
+
+
  
  
